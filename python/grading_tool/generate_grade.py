3ad94090c02400be1ab616fc2221cedd0383ac8f
Yue Gan <gany.fml@gmail.com>
2015-10-22 18:17:19 -0500

    grade generator finished and tested

--- python/grading_tool/generate_grade.py
+++ python/grading_tool/generate_grade.py
@@ -11,45 +11,41 @@ path = "/home/gany/Desktop/Week " + week + "/"
 answer_path = path + 'combined_file.xls'
 grade_path = path + 'grade.mkd'
 
-student_name = []
-student_num_wrong = []
 files = []
 
+#Check if the path exists
 if not os.path.exists(path):
     print "Folder not exist, program exit"
     quit()
 
+#Collect all the students` files
 for f in os.listdir(path): 
     if f.split('.')[-1] == 'xlsx':
         files.append(f)
-        student_name.append(f.split('.')[0].split('_')[1])
 
+#Get the right result from combination.xls
 answer_map = smart_dict()
 answer = xlrd.open_workbook(answer_path).sheet_by_index(0)
-data = [answer.cell_value(row, 2) for row in range(0, answer.nrows)]
+data = [answer.cell_value(row, 2) for row in range(answer.nrows)]
 for index, value in enumerate(data):
     content = value.encode('ascii','ignore').lower()
     if content:
-        if 'n' in content:
+        if 'n' in content[0]:
             answer_map[index] = 'n'
-        elif 'y' in content:
+        elif 'y' in content[0]:
             answer_map[index] = 'y'
-num_question = len(answer_map) - 1; # First row is row description
 
+#Collect the num of wrong answers for each student
+grade_file = open(grade_path, 'w')
 for index in range(0, len(files)):
+    grade_file.write('#' + files[index].split('.')[0].split('_')[1] + '\n' + '- Wrong question index: ')
     worksheet = xlrd.open_workbook(path + files[index]).sheet_by_index(0)
     data = [worksheet.cell_value(row, 2) for row in range(worksheet.nrows)]
     current_num_wrong = 0
     for index, value in enumerate(data):
         content = value.encode('ascii','ignore').lower()
-        #print content + "," + str(answer_map[index])
         if answer_map[index] != 0:
-            if (content and (answer_map[index] not in content)) or (('n' or 'y') not in content):
+            if (not content) or (answer_map[index] not in content):
                 current_num_wrong += 1
-    student_num_wrong.append(current_num_wrong)
-
-grade_file = open(grade_path, 'w')
-for index in range(0, len(files)):
-    grade_file.write('#' + student_name[index] + '\n')
-    grade_file.write('- num of wrong: ' + str(student_num_wrong[index]))
-    grade_file.write('\n')
+                grade_file.write(str(index + 1) + ',')
+    grade_file.write('\n- num of wrong: ' + str(current_num_wrong) + '\n\n')
