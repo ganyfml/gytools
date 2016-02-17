63d04f693c23a59c10e8a5046dda4301568aa651
Yue Gan <gany.fml@gmail.com>
2016-02-16 22:07:34 -0600

    style changed

--- bash/using_vim/main.sh
+++ bash/using_vim/main.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 #The purpose for this program is getting the rare disease list from nih
 
-for i in {A..Z}
+for i in {A..Z} 0-9
 do
   wget https://rarediseases.info.nih.gov/gard/browse-by-first-letter/"$i"
   vim -c "source vim_script" "$i" 
 done
-wget https://rarediseases.info.nih.gov/gard/browse-by-first-letter/0-9
-vim -c "source vim_script" "0-9"
 cat {A..Z} 0-9 > rare_disease_list.mkd
 rm -f {A..Z} 0-9
 zip rare_disease_list.zip rare_disease_list.mkd
