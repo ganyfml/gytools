995d198ce84acb9588ab65d928a27d567d36ca7e
Yue Gan <gany.fml@gmail.com>
2016-03-08 12:47:21 -0600

    Amazon query script refined

--- python/project/collect_tool/amazon/amazonGraphicCardQuery.py
+++ python/project/collect_tool/amazon/amazonGraphicCardQuery.py
@@ -13,7 +13,25 @@ amazon_url = 'http://www.amazon.com/s/ref=lp_284822_pg_2?rh=n%3A172282%2Cn%3A%21
 def get_url_byPageNum(baseUrl, pageNum):
     return baseUrl.replace('page_num', str(pageNum))
 
-def get_products_byUrl(url):
+def get_product_title(soup):
+    return soup.find('span', { 'id' : 'productTitle' }).text
+
+def get_product_price(soup):
+    return soup.find('span', { 'id' : 'priceblock_ourprice' }).text
+
+def get_product_rate(soup):
+    print soup.find('div', { 'id' : 'averageCustomerReviews' }).find('i', { 'class' : 'a-icon a-icon-star a-star-4-5'}).text
+
+def get_review_url(soup):
+    return soup.find('div', { 'id' : 'customer-reviews_feature_div' }).find('div', { 'id' : 'revF' }).find('a')['href'] + '&pageNumber=page_num&reviewerType=avp_only_reviews'
+
+def get_reviews(soup):
+    review_soup = BeautifulSoup(requests.get(get_review_url(soup), headers = headers).text, "html.parser")
+    reviews = review_soup.find_all('div', { 'class' : 'a-section review' })
+    for review in reviews:
+        print review.find('span', { 'class' : 'a-size-base review-text'}).text
+
+def get_products(url):
     base_page_html = requests.get(get_url_byPageNum(url, 1), headers = headers)
     soup = BeautifulSoup(base_page_html.text, "html.parser")
     for page in range(1, 2):
@@ -23,16 +41,9 @@ def get_products_byUrl(url):
         try:
             for product in products:
                 product_soup = BeautifulSoup(requests.get(product.find('a', { 'class' : 'a-link-normal a-text-normal'})['href'], headers = headers).text, "html.parser")
-                title = get_product_title(product_soup)
-                price = get_product_price(product_soup)
-                print title + ', ' + price
+                get_reviews(product_soup)
+                
         except:
-            pass
+            print sys.exc_info()[0]
 
-def get_product_title(soup):
-    return soup.find('span', { 'id' : 'productTitle' }).text
-
-def get_product_price(soup):
-    return soup.find('span', { 'id' : 'priceblock_ourprice' }).text
-
-get_products_byUrl(amazon_url)
+get_products(amazon_url)
