0ffb10f1bb6a48302c3eff537e3b045ebf9e0d5a
Yue Gan <gany.fml@gmail.com>
2016-04-15 18:50:58 -0500

    code improved

--- python/project/collect_tool/gard_rare_disease/gard_rare_disease.py
+++ python/project/collect_tool/gard_rare_disease/gard_rare_disease.py
@@ -1,30 +1,24 @@
 #!/usr/bin/env python
+# vim: set noexpandtab tabstop=2 shiftwidth=2 softtabstop=-1 fileencoding=utf-8:
 
 import requests
 import string
 from bs4 import BeautifulSoup
-import sys
-import codecs
-import locale
+import os
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+def print_diseases_fromUrl(url):
+	disease_list_page = requests.get(url, headers = HEADERS)
+	soup = BeautifulSoup(disease_list_page.text, 'html.parser')
+	diseases = soup.find_all('td', { 'class' : 'DiseaseList' })
+	for d in diseases:
+		disease_name = d.text.strip()
+		if '- See' not in disease_name:
+			print disease_name.encode('utf-8')
 
 #GARD
-URL = 'https://rarediseases.info.nih.gov/gard/browse-by-first-letter/'
-
-def get_diseases_byUrl(url):
-    disease_list_page = requests.get(url, headers = headers)
-    soup = BeautifulSoup(disease_list_page.text, 'html.parser')
-    diseases = soup.find_all('td', { 'class' : 'DiseaseList' })
-    for d in diseases:
-        disease_name = d.text.strip()
-        if '- See' not in disease_name:
-            print disease_name
-
-sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
-reload(sys)
-sys.setdefaultencoding('utf8')
-
-disease_range = list(string.ascii_lowercase) + ['0-9']
-for d in disease_range:
-    get_diseases_byUrl(URL + d)
+url = 'https://rarediseases.info.nih.gov/gard/browse-by-first-letter'
+disease_pages = list(string.ascii_lowercase) + ['0-9']
+for p in disease_pages:
+	print_diseases_fromUrl(os.path.join(url, p))
--- python/project/collect_tool/nord_rare_disease/nord_rare_disease.py
+++ python/project/collect_tool/nord_rare_disease/nord_rare_disease.py
@@ -1,32 +1,26 @@
 #!/usr/bin/env python
+# vim: set noexpandtab tabstop=2 shiftwidth=2 softtabstop=-1 fileencoding=utf-8:
 
 import requests
 from bs4 import BeautifulSoup
-import sys
-import codecs
-import locale
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 #NORD
-URL = "http://rarediseases.org/for-patients-and-families/information-resources/rare-disease-information"
+NORD_URL = "http://rarediseases.org/for-patients-and-families/information-resources/rare-disease-information"
 
 def get_num_pages(url):
-    disease_list_page = requests.get(url, headers = headers)
-    soup = BeautifulSoup(disease_list_page.text, "html.parser")
-    page_indexs = soup.find('ul', { 'class' : 'pagination' }).find_all('li')
-    return int(page_indexs[len(page_indexs) - 2].text)
+	disease_list_page = requests.get(url, headers = HEADERS)
+	soup = BeautifulSoup(disease_list_page.text, "html.parser")
+	page_indexs = soup.find('ul', { 'class' : 'pagination' }).find_all('li')
+	return int(page_indexs[len(page_indexs) - 2].text)
 
-def get_diseases_byUrl(url):
-    disease_list_page = requests.get(url, headers = headers)
-    soup = BeautifulSoup(disease_list_page.text, "html.parser")
-    diseases = soup.find_all("article")
-    for d in diseases:
-       print d.find('a').text
+def print_diseases_fromUrl(url):
+	disease_list_page = requests.get(url, headers = HEADERS)
+	soup = BeautifulSoup(disease_list_page.text, "html.parser")
+	diseases = soup.find_all("article")
+	for d in diseases:
+		print d.find('a').text.encode('utf-8')
 
-sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
-reload(sys)
-sys.setdefaultencoding('utf8')
-
-for i in range(1, get_num_pages(URL) + 1):
-    get_diseases_byUrl('%s/page/%s/' % (URL, i))
+for i in range(get_num_pages(NORD_URL)):
+	print_diseases_fromUrl('%s/page/%s/' % (NORD_URL, i+1))
