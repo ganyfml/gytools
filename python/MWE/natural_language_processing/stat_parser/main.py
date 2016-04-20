29e3a107e81db938446a0c3a07297dfab54f5ae5
Yue Gan <gany.fml@gmail.com>
2016-04-19 21:21:43 -0500

    New MWE added

---
+++ python/MWE/natural_language_processing/stat_parser/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# vim: set noexpandtab tabstop=2 shiftwidth=2 softtabstop=-1 fileencoding=utf-8:
+
+import sys
+from stat_parser import Parser
+
+parser = Parser()
+print parser.parse(sys.argv[1])
---
+++ python/MWE/natural_language_processing/stat_parser/main.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#vim: set noexpandtab tabstop=2:
+
+./main.py "Today is a very good and sunny day"
