75e1541c61426d82491dac5ad9930a16adc6c497
Yue Gan <gany.fml@gmail.com>
2016-06-23 23:40:46 -0500

    google drive search added

--- python/project/collect_tool/google/google_drive/googleDrive_get_file_list/main/main.sh
+++ python/project/collect_tool/google/google_drive/googleDrive_get_file_list/main/main.sh
@@ -2,4 +2,4 @@
 # vim: set noexpandtab tabstop=2:
 
 set -v
-../googleDrive_get_file_list.py "/var/folders/mt/4tg5cc6913v1d1nmjlk2j10c0000gp/T/tmp.MMIlv0EmuP/credential.json"
+../googleDrive_get_file_list.py "../../token"
